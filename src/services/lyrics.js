46864e18f79aefbce2e53767eaddb11e3af228a9
Walter Scarano <wscarano@artear.com>
2017-10-15 10:46:49 -0300

    No need to send headers

--- src/services/lyrics.js
+++ src/services/lyrics.js
@@ -1,6 +1,5 @@
 
 const getById = (lyricsRef) => (req, res, next)=>{
-  res.setHeader("content-type", "application/json");
   lyricsRef.child(req.params.id).once("value")
     .then(response=>response.val())
     .then(obj => res.send(obj))
@@ -8,7 +7,6 @@ const getById = (lyricsRef) => (req, res, next)=>{
 }
 
 const get = (lyricsRef) => (req, res, next)=>{
-  res.setHeader("content-type", "application/json");
   lyricsRef.once("value")
     .then(response=>response.val())
     .then(obj => res.send(obj))
@@ -16,21 +14,18 @@ const get = (lyricsRef) => (req, res, next)=>{
 }
 
 const insert = (lyricsRef) => (req, res, next)=>{
-  res.setHeader("content-type", "application/json");
   lyricsRef.push(req.body)
     .then((response)=>res.send(response))
     .catch(err => res.send(err));
 }
 
 const update = (lyricsRef) => (req, res, next)=>{
-  res.setHeader("content-type", "application/json");
   lyricsRef.child(req.params.id).set(req.body)
     .then((response)=>res.send(response))
     .catch(err => res.send(err));
 }
 
 const remove = (lyricsRef) => (req, res, next)=>{
-  res.setHeader("content-type", "application/json");
   lyricsRef.child(req.params.id).remove()
     .then((response)=>res.send(response))
     .catch(err => res.send(err));
